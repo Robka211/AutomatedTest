4225b72b4ab055794c17a1a3b38a87bfa9ed1c13
Robka211 <58327621+Robka211@users.noreply.github.com>
2019-12-01 09:22:26 +0200

    New auto test

---
+++ New project 2019/New project 2019/Test/ClearTexBox.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace New_project_2019.Test
+{
+    public class ClearTexBox
+    {
+        public void Clear()
+
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            driver.FindElement(By.Name("q")).SendKeys("How to find a cat");
+            Thread.Sleep(2000);
+            driver.FindElement(By.Name("q")).Clear();
+            Thread.Sleep(3000);
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
---
+++ New project 2019/New project 2019/Test/googlePrivacy.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace New_project_2019.Test
+{
+    class googlePrivacy
+    {
+        public void Privacy()
+
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            driver.FindElement(By.XPath("//a[contains(text(),'Privatumas')]")).Click();
+            Thread.Sleep(3000);
+            driver.Navigate().Back();
+            Thread.Sleep(3000);
+            driver.Close();
+        }
+    }
+        }
+
